a44196537a8faefeff8544378e3a86b561872f1e
Muhammad Sufian <itzeditz030@gmail.com>
2026-09-05 16:50:12 +0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
 @echo off
-cd /d "E:\Deepvision.ai\Production-Ready Multi-Agent RAG System"
+cd /d "YOUR_PROJECT_FOLDER_PATH"
 call venv\Scripts\activate.bat
 set PYTHONPATH=backend
-set HF_HOME=E:\Deepvision.ai\Production-Ready Multi-Agent RAG System\hf_cache
-set SENTENCE_TRANSFORMERS_HOME=E:\Deepvision.ai\Production-Ready Multi-Agent RAG System\hf_cache
-set TRANSFORMERS_CACHE=E:\Deepvision.ai\Production-Ready Multi-Agent RAG System\hf_cache
+set HF_HOME=E:YOUR_PROJECT_FOLDER_PATH\hf_cache
+set SENTENCE_TRANSFORMERS_HOME=E:YOUR_PROJECT_FOLDER_PATH\hf_cache
+set TRANSFORMERS_CACHE=E:YOUR_PROJECT_FOLDER_PATH\hf_cache
 uvicorn app.main:app --host 0.0.0.0 --port 8000
